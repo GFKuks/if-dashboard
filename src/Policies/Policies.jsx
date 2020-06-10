29b9ddebc17733c2ba1730222e7173406c486cc7
Gustavs Kuks <Gustavs.Kuks@dtg.lv>
2020-06-10 23:19:34 +0300

    Vertical align, icons, button started

--- src/Policies/Policies.jsx
+++ src/Policies/Policies.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 
+function ActionButton() {
+    return (
+        <div className="button-group">
+            <button className="action-button" type="button" aria-label="Action Select">Fill a claim</button>
+            <button className="dropdown-button" type="button" aria-label="Action Dropdown"><span className="caret-down" /></button>
+        </div>
+    );
+}
+
 export default function Function() {
     const data = [
         {
+            id: 0,
             type: 'Travel',
             details: 'Europe, 1 person',
             date: 'Apr 20, 2019 - Apr 21, 2020',
@@ -10,6 +20,7 @@ export default function Function() {
             action: 'Fill a claim',
         },
         {
+            id: 1,
             type: 'Private property',
             details: 'Zigfrīda Annas Meireovica bulvāris 10-8, Rīga',
             date: 'Jan 6, 2019 - Jan 6, 2020',
@@ -17,6 +28,7 @@ export default function Function() {
             action: 'Fill a claim',
         },
         {
+            id: 2,
             type: 'KASKO',
             details: 'Volvo XS60, FF-4224',
             date: 'Mai 12, 20118 - Mai 12, 2019',
@@ -24,6 +36,7 @@ export default function Function() {
             action: 'Renew',
         },
         {
+            id: 3,
             type: 'PET',
             details: 'Muris',
             date: 'Mai 12, 2019 - Mai 12, 2019',
@@ -34,14 +47,13 @@ export default function Function() {
 
     return (
         <div className="if">
-            <h2 className="if mb-3">Insurance policies</h2>
+            <h2 className="if font weight-54 table-header">Insurance policies</h2>
             <div className="if seperator" />
             <table className="if table">
                 <tbody className="if">
                     { data.map((entry) => {
                         let icon;
                         if (entry.type === 'Travel') {
-                            console.log('travel');
                             icon = 'plane';
                         } else if (entry.type === 'Private property') {
                             icon = 'home';
@@ -51,26 +63,22 @@ export default function Function() {
                             icon = 'paw';
                         }
                         return (
-                            <tr className="if">
-                                <td className="if col-1--sm-">
-                                    <div className="if">
-                                        <img className="policy-icon" alt="Policy Icon" src={`/icons/${icon}.svg`} />
-                                    </div>
+                            <tr key={entry.id} className="if">
+                                <td className="if col-1--sm icon-cell">
+                                    <i className={`if icon product ${icon} brown`} alt="Policy Icon" aria-hidden="true" />
                                 </td>
-                                <td className="if col-5--sm-">
-                                    <div className="if type-text">{entry.type}</div>
-                                    <div className="if details-text">{entry.details}</div>
+                                <td className="if col-4--sm">
+                                    <div className="if font weight-126 type-text">{entry.type}</div>
+                                    <div className="if font weight-70 details-text">{entry.details}</div>
                                 </td>
-                                <td className="if col-2--sm-">
-                                    <p className="if date-text float-right">{entry.date}</p>
+                                <td className="if col-3--sm">
+                                    <div className="if font weight-70 date-text">{entry.date}</div>
                                 </td>
-                                <td className="if col-2--sm-">
-                                    <p className={`if status ${entry.status} float-right`}>{entry.status}</p>
+                                <td className="if col-2--sm">
+                                    <div className={`if font weight-126 status ${entry.status}`}>{entry.status}</div>
                                 </td>
-                                <td className="if col-2--sm-">
-                                    <button type="button" className="if button primary small">
-                                        Button
-                                    </button>
+                                <td className="if col-2--sm action-cell">
+                                    <ActionButton />
                                 </td>
                             </tr>
                         );
